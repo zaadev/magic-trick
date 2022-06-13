613f985b3b4746bedba6ef70f999130643cb918a
zaadev <yea1212#!>
2022-06-13 15:49:57 +0500

    Conflict fixes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 Building Multi Steps form in VueJS and Tailwind CSS
 =============================================
 
@@ -23,3 +24,29 @@ Website & links
 * Follow me on Twitter - [https://twitter.com/manojnegiwd](https://twitter.com/manojnegiwd)
 
 * Recraft Relic website - [https://recraftrelic.com/](https://recraftrelic.com/)
+=======
+# magic_trick
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+>>>>>>> a2aa3a4 (init)
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
+<<<<<<< HEAD
   <MagicMathCards />
 </template>
 
 <script>
 import MagicMathCards from './components/MagicMathCards.vue';
 
+=======
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+>>>>>>> a2aa3a4 (init)
 
 export default {
   name: 'App',
   components: {
+<<<<<<< HEAD
     MagicMathCards
+=======
+    HelloWorld
+>>>>>>> a2aa3a4 (init)
   }
 }
 </script>
@@ -19,7 +32,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+<<<<<<< HEAD
   margin-top: 60px;
   margin-bottom: 60px;
+=======
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+>>>>>>> a2aa3a4 (init)
 }
 </style>
